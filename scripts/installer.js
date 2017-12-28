7a7fafd3b976558e00474c22bbfeeb0cfe89f208
Peter Soboyejo <thepcmrtim@gmail.com>
2017-12-27 23:48:56 -0500

    Prod Builder

---
+++ scripts/installer.js
@@ -0,0 +1,34 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+const rimraf = require('rimraf')
+
+deleteOutputFolder()
+  .then(getInstallerConfig)
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.log('err')
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  const rootPath = path.join(__dirname, '..')
+  const outPath = path.join(rootPath, 'dist')
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'win-unpacked'),
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    setupExe: 'SupremeAccessSetup.exe',
+    loadingGif: path.join(rootPath, 'static', 'loading.gif'),
+    setupIcon: path.join(rootPath, 'static', 'icons', 'logo.ico'),
+    skipUpdateIcon: true
+  })
+}
+
+function deleteOutputFolder () {
+  return new Promise((resolve, reject) => {
+    rimraf(path.join(__dirname, '..', 'out', 'windows-installer'), (error) => {
+      error ? reject(error) : resolve()
+    })
+  })
+}
